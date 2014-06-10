81583b9161295c5d24b3e2dcccf9ccd76e0cf03f
portOdin <dominic.n@me.com>
2014-06-10 14:29:03 -0400

    added selector. figuring out how to route to correct index view depending on the selected option. going to try to install simple form.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140609022045) do
+ActiveRecord::Schema.define(version: 20140609224552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -172,6 +172,7 @@ ActiveRecord::Schema.define(version: 20140609022045) do
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
+    t.string   "favortype"
   end
 
   add_index "requests", ["user_id"], name: "index_requests_on_user_id", using: :btree
