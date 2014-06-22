9ece431d5976069cfbf1b0e293be7ca3c07dff46
portOdin <dominic.n@me.com>
2014-06-22 15:39:00 -0400

    going to work on profile page for profile pic editing.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -36,22 +36,8 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.delivery_method = :smtp
 
-  Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
-  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.delivery_method = :sendmail
 
-  ActionMailer::Base.smtp_settings = {
-		  :domain =>  'localhost:3000',
-      :address => "smtp.gmail.com",
-		  :port => 587,
-		  :user_name => 'blur606@gmail.com',
-		  :password => 'element9',
-		  :authentication => 'plain',
-		  :enable_starttls_auto => true
-  }
 end
